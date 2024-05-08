97d119f0f71883658e8cc7d3ede19f55a246350d
Jose Garayar <jgarayarp@uni.pe>
2024-05-08 00:24:52 -0500

    fix in flask file and index html, added notebook

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ model = pickle.load(open('pickle_files/model.pkl','rb'))
 # crear flask
 app = Flask(__name__)
 
-def prediction(gender, ssc_p, hsc_p, degree_p, workex, etest_p, specialisation, mba_p):
-    gender = 0 if "F" else 0
-    workex = 1 if "Yes" else 0
-    specialisation = 0 if "Mkt&Fin" else 1
-    data = [[gender, ssc_p, hsc_p, degree_p, workex, etest_p, specialisation, mba_p]]
-    prediction = model.predict(data)
-    return prediction
-
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -29,6 +21,14 @@ def predict():
     etest_p = request.form['etest_p']
     specialisation = request.form['specialisation']
     mba_p = request.form['mba_p']
+
+    def prediction(gender, ssc_p, hsc_p, degree_p, workex, etest_p, specialisation, mba_p):
+        gender = 0 if "F" else 0
+        workex = 1 if "Yes" else 0
+        specialisation = 0 if "Mkt&Fin" else 1
+        data = [[gender, ssc_p, hsc_p, degree_p, workex, etest_p, specialisation, mba_p]]
+        prediction = model.predict(data)
+        return prediction
 
     prediction = prediction(gender, ssc_p, hsc_p, degree_p, workex, etest_p, specialisation,mba_p)
 
